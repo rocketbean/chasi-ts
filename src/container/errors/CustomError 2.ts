80adf9ad839259f8cf3988c977f01924e5789e5d
Nikko Mesina <buzzrocketbean@gmail.com>
2024-10-31 11:51:33 +0800

    v2.3.9

--- src/container/errors/CustomError 2.ts
+++
@@ -1,6 +0,0 @@
-export default class CustomError extends Error {
-  constructor (public message: string, public status?: number) {
-    super(message)
-    if(!this.status) this.status = 500
-  }
-}
