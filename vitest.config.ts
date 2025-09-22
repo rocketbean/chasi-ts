f3c27bdbd03b4cf3e6a2088837c45db767f24ea2
Nikko Mesina <buzzrocketbean@gmail.com>
2025-09-22 17:32:04 +0800

    update config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,21 @@
 /// <reference types="vitest" />
 import { defineConfig} from 'vitest/config'
 import { loadEnv } from 'vite'
+import { resolve, dirname } from "node:path";
 
 export default defineConfig({
+  resolve: {},
   test: {
-    exclude: ['dist/*', "node_modules"],
+    exclude: ["dist", "node_modules", "src"],
+    includeSource: ["src/types"],
     globals: true,
-    environment: 'node',
-    passWithNoTests: true,
-    env: loadEnv('test', process.cwd() + "/test", "")
+    environment: "node",
+    env: loadEnv("test", process.cwd() + "/test", ""),
+    testTimeout: 20000,
+    typecheck: {
+      enabled: false,
+      ignoreSourceErrors: true,
+      tsconfig: "tsconfig.json",
+    },
   },
-})
+});
